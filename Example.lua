5f23e0c5a6b114c66ca4354675c688a638dcc0ea
zenoXD sh <angeemmanuelglarou5@gmail.com>
2025-09-01 09:32:53 +0200

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -3,7 +3,7 @@ local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Git-a
 local Window = Library:CreateWindow({
     Title = "Player Utilities",
     Size = UDim2.fromOffset(600, 400),
-    Transparency = 0.1,
+    Transparency = 0.15,
     Blurring = true,
     Theme = "Void"
 })
